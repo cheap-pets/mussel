76dba624bf26946e209811959c402d14225b2455
scrollbar <scrollbar@163.com>
2019-09-19 12:05:59 +0800

    adjust shadow variables

--- src/style-variables.js
+++ src/style-variables.js
@@ -6,10 +6,11 @@ const grey = generate('#b2b2b2')
 '#b2b2b2', '#8c8c8c', '#666666', '#404040', '#1a1a1a'
 */
 let _primaryColor, _successColor, _warningColor, _dangerColor, _infoColor
+let _primaryColors, _successColors, _dangerColors
 
-const buttonShadow = 'none'
-const buttonHoverShadow = '0 0 0 0.2rem $$shadowColor'
-const buttonActiveShadow = 'none'
+let _buttonShadow = 'none'
+let _buttonHoverShadow = '0 0 0 0.2rem $$shadowColor'
+let _buttonActiveShadow = 'none'
 
 const variables = {
   /* document */
@@ -21,16 +22,13 @@ const variables = {
   buttonXPaddingPx: 10,
   buttonBorderRadiusPx: 2,
   buttonFontSize: '1rem',
-  buttonShadow,
-  buttonHoverShadow,
-  buttonActiveShadow,
   buttonDefaultColor: grey[7],
   buttonDefaultHoverColor: grey[6],
   buttonDefaultActiveColor: grey[8],
   buttonDefaultDisabledColor: grey[4],
-  buttonDefaultShadow: buttonShadow.replace('$$shadowColor', grey[3]),
-  buttonDefaultHoverShadow: buttonHoverShadow.replace('$$shadowColor', grey[3]),
-  buttonDefaultActiveShadow: buttonActiveShadow.replace('$$shadowColor', grey[4]),
+  buttonDefaultShadow: _buttonShadow.replace('$$shadowColor', grey[2]),
+  buttonDefaultHoverShadow: _buttonHoverShadow.replace('$$shadowColor', grey[2]),
+  buttonDefaultActiveShadow: _buttonActiveShadow.replace('$$shadowColor', grey[4]),
 
   /* input */
   inputHeightPx: 32,
@@ -72,26 +70,43 @@ const variables = {
 
 }
 
+function setButtonShadow (buttonType, shadowType) {
+  const colors = {
+    Default: grey,
+    Primary: _primaryColors,
+    Submit: _successColors,
+    Danger: _dangerColors
+  }
+
+  Object.keys(colors).forEach(key => {
+    if (!buttonType || buttonType === key) {
+      const color = [...colors[key]]
+      // if (buttonType === 'Default') color.splice(0, 1)
+      if (!shadowType || shadowType === 'normal') {
+        variables[`button${key}Shadow`] =
+          variables.buttonShadow.replace('$$shadowColor', color[2])
+      }
+
+      if (!shadowType || shadowType === 'hover') {
+        variables[`button${key}HoverShadow`] =
+          variables.buttonHoverShadow.replace('$$shadowColor', color[2])
+      }
+
+      if (!shadowType || shadowType === 'active') {
+        variables[`button${key}ActiveShadow`] =
+          variables.buttonActiveShadow.replace('$$shadowColor', color[4])
+      }
+    }
+  })
+}
+
 function setButtonColors (buttonType, colors) {
   variables[`button${buttonType}Color`] = colors[5]
   variables[`button${buttonType}HoverColor`] = colors[4]
   variables[`button${buttonType}ActiveColor`] = colors[6]
   variables[`button${buttonType}DisabledColor`] = colors[2]
 
-  const {
-    buttonShadow,
-    buttonHoverShadow,
-    buttonActiveShadow
-  } = variables
-
-  variables[`button${buttonType}Shadow`] =
-    buttonShadow.replace('$$shadowColor', colors[2])
-
-  variables[`button${buttonType}HoverShadow`] =
-    buttonHoverShadow.replace('$$shadowColor', colors[2])
-
-  variables[`button${buttonType}ActiveShadow`] =
-    buttonActiveShadow.replace('$$shadowColor', colors[4])
+  setButtonShadow(buttonType)
 }
 
 Object.defineProperties(variables, {
@@ -101,10 +116,10 @@ Object.defineProperties(variables, {
     },
     set (v) {
       _primaryColor = v
-      const colors = generate(v)
-      setButtonColors('Primary', colors)
-      this.inputShadowColor = colors[2]
-      this.inputHoverBorderColor = colors[5]
+      _primaryColors = generate(v)
+      setButtonColors('Primary', _primaryColors)
+      this.inputShadowColor = _primaryColors[2]
+      this.inputHoverBorderColor = _primaryColors[5]
     },
     enumerable: true
   },
@@ -114,8 +129,8 @@ Object.defineProperties(variables, {
     },
     set (v) {
       _successColor = v
-      const colors = generate(v)
-      setButtonColors('Submit', colors)
+      _successColors = generate(v)
+      setButtonColors('Submit', _successColors)
     },
     enumerable: true
   },
@@ -134,10 +149,10 @@ Object.defineProperties(variables, {
     },
     set (v) {
       _dangerColor = v
-      const colors = generate(v)
-      setButtonColors('Danger', colors)
-      this.inputInvalidShadowColor = colors[2]
-      this.inputInvalidBorderColor = colors[5]
+      _dangerColors = generate(v)
+      setButtonColors('Danger', _dangerColors)
+      this.inputInvalidShadowColor = _dangerColors[2]
+      this.inputInvalidBorderColor = _dangerColors[5]
     },
     enumerable: true
   },
@@ -147,6 +162,36 @@ Object.defineProperties(variables, {
     },
     set (v) {
       _infoColor = v
+    },
+    enumerable: true
+  },
+  buttonShadow: {
+    get () {
+      return _buttonShadow
+    },
+    set (v) {
+      _buttonShadow = v
+      setButtonShadow(null, 'normal')
+    },
+    enumerable: true
+  },
+  buttonHoverShadow: {
+    get () {
+      return _buttonHoverShadow
+    },
+    set (v) {
+      _buttonHoverShadow = v
+      setButtonShadow(null, 'hover')
+    },
+    enumerable: true
+  },
+  buttonActiveShadow: {
+    get () {
+      return _buttonActiveShadow
+    },
+    set (v) {
+      _buttonActiveShadow = v
+      setButtonShadow(null, 'active')
     },
     enumerable: true
   }
