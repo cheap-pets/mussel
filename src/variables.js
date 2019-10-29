68088eaa18f1e3513f1158099a0450371d6ae27f
scrollbar-ww <scrollbar@163.com>
2019-10-29 22:34:42 +0800

    new color vars

---
+++ src/variables.js
@@ -0,0 +1,51 @@
+import { generate } from '@ant-design/colors'
+
+const grey = generate('#b2b2b2')
+
+/* primary */
+const primaryColor = ''
+const primaryHoverColor = ''
+const primaryActiveColor = ''
+const primaryShadowColor = ''
+
+/* success */
+const successColor = ''
+const successHoverColor = ''
+const successActiveColor = ''
+const successShadowColor = ''
+
+/* danger */
+const dangerColor = ''
+const dangerHoverColor = ''
+const dangerActiveColor = ''
+const dangerShadowColor = ''
+
+/* warning */
+const warningColor = ''
+
+/* info */
+const infoColor = ''
+
+export default {
+  /* colors */
+
+  /* primary */
+  primaryColor,
+  primaryHoverColor,
+  primaryActiveColor,
+  primaryShadowColor,
+  /* success */
+  successColor,
+  successHoverColor,
+  successActiveColor,
+  successShadowColor,
+  /* danger */
+  dangerColor,
+  dangerHoverColor,
+  dangerActiveColor,
+  dangerShadowColor,
+  /* warning */
+  warningColor,
+  /* info */
+  infoColor
+}
