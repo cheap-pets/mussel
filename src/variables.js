b5c11e0601e37d6a4bdd01dc690ba7874752d8ac
scrollbar-ww <scrollbar@163.com>
2019-10-29 22:41:06 +0800

    + disabled color

--- src/variables.js
+++ src/variables.js
@@ -7,24 +7,31 @@ const primaryColor = ''
 const primaryHoverColor = ''
 const primaryActiveColor = ''
 const primaryShadowColor = ''
+const primaryDisabledColor = ''
 
 /* success */
 const successColor = ''
 const successHoverColor = ''
 const successActiveColor = ''
 const successShadowColor = ''
+const successDisabledColor = ''
 
 /* danger */
 const dangerColor = ''
 const dangerHoverColor = ''
 const dangerActiveColor = ''
 const dangerShadowColor = ''
+const dangerDisabledColor = ''
 
 /* warning */
 const warningColor = ''
 
 /* info */
 const infoColor = ''
+
+const normalHoverColor = ''
+const normalBorderColor = ''
+const normalDisabledColor = ''
 
 export default {
   /* colors */
@@ -34,16 +41,19 @@ export default {
   primaryHoverColor,
   primaryActiveColor,
   primaryShadowColor,
+  primaryDisabledColor,
   /* success */
   successColor,
   successHoverColor,
   successActiveColor,
   successShadowColor,
+  successDisabledColor,
   /* danger */
   dangerColor,
   dangerHoverColor,
   dangerActiveColor,
   dangerShadowColor,
+  dangerDisabledColor,
   /* warning */
   warningColor,
   /* info */
