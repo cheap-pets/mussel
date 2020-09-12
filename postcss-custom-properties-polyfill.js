e57a64770fe012904912091629d604d98ed71fa9
scrollbar-ww <scrollbar@163.com>
2020-09-12 16:48:35 +0800

    🐛: fix bug that transform varibales when bundling

--- postcss-custom-properties-polyfill.js
+++ postcss-custom-properties-polyfill.js
@@ -2,11 +2,17 @@ import postcss from 'postcss'
 
 function transformDecl (decl, variables) {
   if (decl.prop.indexOf('--') === 0) variables[decl.prop] = decl.value
-  const s = decl.value.trim()
-  const idx = s.indexOf('var(')
-  if (idx === 0 && s.length > 7 && s.indexOf(')') === s.length - 1) {
-    const v = s.substr(4, s.length - 5).trim()
-    if (variables[v]) decl.cloneBefore({ value: variables[v] })
+
+  const s = decl.value
+  const startIdx = s.indexOf('var(')
+  const endIdx = startIdx === -1 ? -1 : s.indexOf(')', startIdx)
+  const variable = endIdx === -1 ? null : s.substring(startIdx + 4, endIdx)
+  const value = variable ? variables[variable.trim()] : null
+
+  if (value) {
+    decl.cloneBefore({
+      value: s.replace(s.substring(startIdx, endIdx + 1), value)
+    })
   }
 }
 
