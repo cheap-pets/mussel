5fa703c8925559c591bf6049dc8ac78a08da375a
scrollbar-ww <scrollbar@163.com>
2020-09-29 11:41:42 +0800

    🐛: replace size attribute of tabler-icons svg dom

--- src/components/icon/tabler-icons.js
+++ src/components/icon/tabler-icons.js
@@ -15,14 +15,19 @@ import chevronRight from '@icons/chevron-right.svg'
 import expand from '@icons/folder-plus.svg'
 import collapse from '@icons/folder-minus.svg'
 
-const SVG_ROOT = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">'
-
-function replaceSvgNode (s) {
+function removeSizeAttribute (s) {
   const startIdx = s.indexOf('<svg')
   const endIdx = startIdx === -1 ? -1 : s.indexOf('>', startIdx)
   const ss = endIdx === -1 ? null : s.substring(startIdx, endIdx + 1)
-  console.log(ss)
-  return ss ? s.replace(ss, SVG_ROOT) : s
+  return ss
+    ? s.replace(
+      ss,
+      ss
+        .replace(/\swidth=".*?"/, '')
+        .replace(/\sheight=".*?"/, '')
+        .replace(/\sclass=".*?"/, '')
+    )
+    : s
 }
 
 const icons = {
@@ -43,7 +48,7 @@ const icons = {
 }
 
 Object.keys(icons).forEach(key => {
-  icons[key] = replaceSvgNode(icons[key])
+  icons[key] = removeSizeAttribute(icons[key])
 })
 
 export default icons
